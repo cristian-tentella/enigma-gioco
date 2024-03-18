91e947badc8bb2d57bf6e2426f6f898d420c7019
Cristian Tentella <cristiantentella@outlook.com>
2024-03-18 22:02:21 +0100

    Update player animation based on latest movement action

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 @export_category("Settings")
 @export_group("Movement constants")
 @export var max_speed: float = 128
 @export var acceleration: float = 8
 @export var friction: float = 16
+
+const MOVEMENT_ACTIONS = ["move_left", "move_right", "move_up", "move_down"]
+var movement_actions_list: Array[String]
 
 
 func get_input() -> Vector2:
@@ -33,6 +38,25 @@ func calculate_new_velocity(
 	return new_velocity
 
 
+func update_animated_sprite():
+	var current_animation_name = animated_sprite.animation
+	
+	var next_animation_name = current_animation_name
+	if not movement_actions_list.is_empty():
+		next_animation_name = movement_actions_list.back()
+
+	animated_sprite.play(next_animation_name)
+
+
 func _physics_process(delta: float):
 	velocity = calculate_new_velocity(velocity, get_input(), delta)
+	update_animated_sprite()
 	move_and_slide()
+
+
+func _unhandled_input(event: InputEvent):
+	for movement_action in MOVEMENT_ACTIONS:
+		if event.is_action_pressed(movement_action):
+			movement_actions_list.append(movement_action)
+		if event.is_action_released(movement_action):
+			movement_actions_list.erase(movement_action)
