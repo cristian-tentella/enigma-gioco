2aa98bd8c8207d2ff30a00c98ac5066ccb022aff
Cristian Tentella <cristiantentella@outlook.com>
2024-03-18 19:20:23 +0100

    Clean up player movement code

--- player/player.gd
+++ player/player.gd
@@ -8,18 +8,31 @@ extends CharacterBody2D
 @export var friction: float = 16
 
 
-func _physics_process(delta: float):
-	var input = Vector2(
+func get_input() -> Vector2:
+	return Vector2(
 		Input.get_axis("move_left", "move_right"),
 		Input.get_axis("move_up", "move_down")
 	)
 
+
+func calculate_new_velocity(
+	current_velocity: Vector2,
+	input: Vector2,
+	delta: float
+) -> Vector2:
+	var new_velocity: Vector2
+
 	if input == Vector2.ZERO:
-		velocity = velocity.lerp(Vector2.ZERO, friction * delta)
+		new_velocity = current_velocity.lerp(Vector2.ZERO, friction * delta)
 	else:
-		velocity = velocity.lerp(
+		new_velocity = current_velocity.lerp(
 			max_speed * input.normalized(),
 			acceleration * delta
 		)
 
+	return new_velocity
+
+
+func _physics_process(delta: float):
+	velocity = calculate_new_velocity(velocity, get_input(), delta)
 	move_and_slide()
