40de2aa41c21827b55a0100dc16773d57ac39a24
Cristian Tentella <cristiantentella@tuta.com>
2024-05-17 23:46:18 +0200

    Refactor player script

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -8,14 +8,79 @@ const pause_menu_scene = preload("res://ui/pause_menu/pause_menu.tscn")
 
 @export_category("Settings")
 @export_group("Movement constants")
-@export var max_speed: float = 128
-@export var acceleration: float = 8
-@export var friction: float = 16
+@export var MAX_SPEED: float = 128
+@export var ACCELERATION: float = 8
+@export var FRICTION: float = 16
 
 var input = Vector2.ZERO
 
 const MOVEMENT_ACTIONS = ["move_left", "move_right", "move_up", "move_down"]
-var movement_actions_list: Array[String]
+var movement_actions_queue: Array[String]
+
+
+func _physics_process(delta: float):
+	update_velocity(delta)
+	move_and_slide()
+
+
+func _unhandled_input(event: InputEvent):
+	update_movement_actions_list(event)
+	handle_remaining_actions(event)
+	update_input_vector()
+	update_animated_sprite()
+	update_interaction_detector_position()
+
+
+func update_velocity(delta: float):
+	if self.input == Vector2.ZERO:
+		self.velocity = self.velocity.lerp(Vector2.ZERO, self.FRICTION * delta)
+	else:
+		self.velocity = self.velocity.lerp(
+			self.MAX_SPEED * self.input.normalized(),
+			self.ACCELERATION * delta
+		)
+
+
+func update_movement_actions_list(event: InputEvent):
+	for movement_action in self.MOVEMENT_ACTIONS:
+		if event.is_action_pressed(movement_action):
+			self.movement_actions_queue.append(movement_action)
+		if event.is_action_released(movement_action):
+			self.movement_actions_queue.erase(movement_action)
+	if not State.should_player_be_able_to_move():
+		self.movement_actions_queue.clear()
+
+
+func update_input_vector():
+	var new_input = Vector2.ZERO
+
+	for movement_action in self.movement_actions_queue:
+		new_input += get_vector_from_movement_action(movement_action)
+
+	self.input = new_input
+
+
+func handle_remaining_actions(event: InputEvent):
+	# TODO: find a better place for this
+	if event.is_action_pressed("interact"):
+		self.interaction_detector.activate_closest_interaction()
+
+
+func update_animated_sprite():
+	var current_animation_name: String = self.animated_sprite.animation
+	
+	var next_animation_name = current_animation_name
+	if not self.movement_actions_queue.is_empty():
+		next_animation_name = latest_movement_action()
+	else:
+		next_animation_name = current_animation_name.replace("move", "idle")
+
+	self.animated_sprite.play(next_animation_name)
+
+
+func update_interaction_detector_position():
+	const offset_multiplier = 24
+	self.interaction_detector.position = input * offset_multiplier
 
 
 func get_vector_from_movement_action(movement_action: String):
@@ -30,74 +95,5 @@ func get_vector_from_movement_action(movement_action: String):
 			return Vector2.DOWN
 
 
-func calculate_input_vector(movement_actions: Array[String]) -> Vector2:
-	var new_input = Vector2.ZERO
-
-	for movement_action in movement_actions:
-		new_input += get_vector_from_movement_action(movement_action)
-
-	return new_input
-
-
 func latest_movement_action() -> String:
-	return movement_actions_list.back()
-
-
-func calculate_new_velocity(
-	current_velocity: Vector2,
-	input_vector: Vector2,
-	delta: float
-) -> Vector2:
-	var new_velocity: Vector2
-
-	if input_vector == Vector2.ZERO:
-		new_velocity = current_velocity.lerp(Vector2.ZERO, friction * delta)
-	else:
-		new_velocity = current_velocity.lerp(
-			max_speed * input_vector.normalized(),
-			acceleration * delta
-		)
-
-	return new_velocity
-
-
-func update_animated_sprite():
-	var current_animation_name: String = animated_sprite.animation
-	
-	var next_animation_name = current_animation_name
-	if not movement_actions_list.is_empty():
-		next_animation_name = latest_movement_action()
-	else:
-		next_animation_name = current_animation_name.replace("move", "idle")
-
-	animated_sprite.play(next_animation_name)
-
-
-func update_interaction_detector_position():
-	const offset_multiplier = 24
-	interaction_detector.position = input * offset_multiplier
-
-
-func _physics_process(delta: float):
-	velocity = calculate_new_velocity(velocity, input, delta)
-	move_and_slide()
-
-
-func _unhandled_input(event: InputEvent):
-	for movement_action in MOVEMENT_ACTIONS:
-		if event.is_action_pressed(movement_action):
-			movement_actions_list.append(movement_action)
-		if event.is_action_released(movement_action):
-			movement_actions_list.erase(movement_action)
-
-	if not State.should_player_be_able_to_move():
-		movement_actions_list.clear()
-
-	input = calculate_input_vector(movement_actions_list)
-
-	# TODO: find a better place for this
-	if event.is_action_pressed("interact"):
-		interaction_detector.activate_closest_interaction()
-
-	update_animated_sprite()
-	update_interaction_detector_position()
+	return self.movement_actions_queue.back()
