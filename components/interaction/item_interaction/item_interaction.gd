6c13e32862bc4da71a66c53304ef575cb6369909
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-06-14 00:27:46 +0200

    Add ItemInteraction variable, lower InteractionDetector size, slight refactor
    Aggiunta la variabile "item_that_is_in_the_interaction" all'interno di handle_interaction di ItemInteraction, che sarà utile in futuro.
    
    Diminuita la dimensione della sfera davanti al giocatore per le interazioni, prima era troppo grande e prendeva troppi oggetti.
    
    Leggero refactor e rimozione di test.

--- components/interaction/item_interaction/item_interaction.gd
+++ components/interaction/item_interaction/item_interaction.gd
@@ -1,7 +1,11 @@
 class_name ItemInteraction
 extends Interaction
 
-#@export var quello_che_ti_serve: tipo_che_ti_serve
+var item_that_is_in_the_interaction: ItemResource
 
 func handle_interaction():
-	pass
+	item_that_is_in_the_interaction = get_parent().item_resource #Questo è l'oggetto ItemResource con cui avviene l'interazione.
+	
+	"""
+	Probabilmente qui ci andrà una cosa tipo StateManager.add_to_inventory(item_that_is_in_the_interaction)
+	"""
--- game/items/generic_item.gd
+++ game/items/generic_item.gd
@@ -16,8 +16,7 @@ Ogni oggetto è dato dalla scena generic_item.tscn
 @onready var sprite2D = $Sprite2D
 var item_resource: ItemResource
 
-func initialize_the_real_item_with_its_properties(item_resource: ItemResource):
-	
+func _initialize_the_real_item_with_its_properties(item_resource: ItemResource):
 	#Cambia il raggio del CollisionShape2D in base al raggio indicato nel file .tres
 	collision_circle_shape.shape.radius = item_resource.collision_circle_radius
 
@@ -29,15 +28,14 @@ func initialize_the_real_item_with_its_properties(item_resource: ItemResource):
 	#Carica l'immagine dell'oggetto
 	var image_texture = load(item_resource.sprite_path)
 	sprite2D.texture = image_texture
-	print("Done!")
-	
 
 
 func _ready():
 	""" 
 	IL NOME DEL NODO NELLA SCENA DEFINISCE L'ITEM CHE VUOI CHE SIA.
 	FAI ATTENZIONE CHE LA FOLDER DELL'ITEM SI CHIAMI nome_item E CHE IL .tres ABBIA LA FORMA nome_item.tres
-	
+	(Segui il tutorial di generic_item.gd e item_resource.gd
+	)
 	esempio:
 		voglio l'item mazzo_di_chiavi :
 			dentro items c'è una folder mazzo_di_chiavi, al cui interno c'è il file mazzo_di_chiavi.tres
@@ -45,5 +43,9 @@ func _ready():
 	"""
 	var node_name = get_name()
 	var tres_path = "res://game/items/" + node_name + "/" + node_name + ".tres"
-	var item_tres = load(tres_path) as ItemResource
-	initialize_the_real_item_with_its_properties(item_tres)
+	
+	item_resource = load(tres_path) as ItemResource
+	_initialize_the_real_item_with_its_properties(item_resource)
+	
+func tester():
+	print("I am a generic item!")
