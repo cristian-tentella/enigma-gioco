00742931f439bae3e89e35776b2f041d9e1c7ec6
Mary <maria.lazzaro@hotmail.com>
2024-06-17 20:22:48 +0200

    Fix typos

--- ui/inventory/inventory_ui.gd
+++ ui/inventory/inventory_ui.gd
@@ -12,7 +12,7 @@ func _ready():
 	inv.update.connect(update_slots)
 	update_slots()
 	
-# Doing it dynamically is the only way to make it work. 
+# Doing it dynamically is the only way to make it work.
 # Original implementation with .tres was overkill considering I just create 8 empty slots with no properties
 func _instantiate_inventory():
 	inv = Inventory.new()
@@ -21,7 +21,6 @@ func _instantiate_inventory():
 func update_slots():
 	for i in range(min(inv.slots.size(), slots_UI.size())):
 		slots_UI[i].update(inv.slots[i])
-		print(slots_UI[i].item_visual.texture)
 
 func _on_exit_pause_menu_button_pressed():
 	self.exit.emit() # Replace with function body.
