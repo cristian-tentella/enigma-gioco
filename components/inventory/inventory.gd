f1d723a3f3832a074bc9660e30c950d64f7fa288
Mary <maria.lazzaro@hotmail.com>
2024-06-18 13:15:15 +0200

    Fix minor warning
    invalid variable name is now valid

--- components/inventory/inventory.gd
+++ components/inventory/inventory.gd
@@ -9,9 +9,9 @@ func insert(item: PickableItem):
 	slots.append(item)
 	update.emit()
 
-func has_item(name: String):
+func has_item(needed_item_name: String):
 	for i in slots:
-		if i.item_name == name:
+		if i.item_name == needed_item_name:
 			return true
 	return false
 		
