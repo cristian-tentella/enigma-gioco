96582c013e6abe21106165c549d73855a0ffbbd6
Giorey01 <giorey2001@gmail.com>
2024-06-18 18:16:46 +0200

    Remove unused signal

--- ui/authentication_menu/authentication_menu.gd
+++ ui/authentication_menu/authentication_menu.gd
@@ -4,7 +4,6 @@ extends Control
 @onready var email = $email
 @onready var password = $password
 
-signal exit
 
 func _on_sign_up_button_pressed():
 	AuthenticationManager.sign_up(email.text, password.text)
