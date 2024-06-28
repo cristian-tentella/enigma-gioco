9f0c2326b4a7e5d92b6a8ffaee3c11494b302d64
Giorey01 <giorey2001@gmail.com>
2024-06-28 14:54:15 +0200

    Add w.i.p. soundtrack player implementation

--- components/audio_player/audio_player.gd
+++ components/audio_player/audio_player.gd
@@ -1,10 +1,11 @@
 class_name AudioPlayer
 extends Node
 
+var audio_stream_player_for_sound_track: AudioStreamPlayer
 
 func _ready():
 	AudioManager.play_sound_effect.connect(_on_audio_manager_play_sound_effect)
-
+	AudioManager.play_sound_track.connect(_on_audio_manager_play_sound_track)
 
 func _on_audio_manager_play_sound_effect(sound_effect_name: String):
 	var audio_stream_player = _convert_sound_effect_name_to_audio_stream_player(sound_effect_name)
@@ -17,15 +18,37 @@ func _on_audio_manager_play_sound_effect(sound_effect_name: String):
 
 	audio_stream_player.play()
 
+func _on_audio_manager_play_sound_track(sound_track_name: String):
+	if !audio_stream_player_for_sound_track.is_playing():
+		audio_stream_player_for_sound_track = _convert_sound_effect_name_to_audio_stream_player(sound_track_name)
+		if audio_stream_player_for_sound_track == null:
+			return
 
+		audio_stream_player_for_sound_track.play()
+	else:
+		push_error("Non puoi suonare due sound track in contemporanea")
+	
+	
 func _convert_sound_effect_name_to_audio_stream_player(sound_effect_name: String) -> AudioStreamPlayer:
-	var audio_stream_player: AudioStreamPlayer
+	var audio_stream_player_for_sound_effects: AudioStreamPlayer
 
 	match sound_effect_name:
 		"click":
-			audio_stream_player = $SoundEffects/Click
+			audio_stream_player_for_sound_effects = $SoundEffects/Click
 		_:
 			push_error("Non esiste alcun effetto sonoro chiamato '{0}'".format([sound_effect_name]))
 			return null
 
-	return audio_stream_player
+	return audio_stream_player_for_sound_effects
+
+
+func _convert_sound_track_name_to_audio_stream_player(sound_track_name: String) -> AudioStreamPlayer:
+	match sound_track_name:
+		"Lullaby":
+			audio_stream_player_for_sound_track = $SoundTracks/Lullaby
+		_:
+			push_error("Non esiste alcun effetto sonoro chiamato '{0}'".format([sound_track_name]))
+			return null
+
+	return audio_stream_player_for_sound_track
+	
--- managers/audio.gd
+++ managers/audio.gd
@@ -1,6 +1,11 @@
 extends Node
 
 signal play_sound_effect(sound_effect_name: String)
+signal play_sound_track(sound_track_name: String)
 
 func play_click_sound_effect():
 	self.play_sound_effect.emit("click")
+
+
+func play_lullaby_sound_track():
+	self.play_sound_track.emit("Lullaby")
