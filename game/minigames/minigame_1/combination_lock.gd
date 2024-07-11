2501fa5ecc310ba00c160e50cbdc11c1eb192c4e
livingmeme03 <maria.lazzaro@hotmail.com>
2024-07-12 00:19:44 +0200

    Create very essential combination lock minigame interface
    With buttons and functions on press

---
+++ game/minigames/minigame_1/combination_lock.gd
@@ -0,0 +1,41 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _on_reset_minigame_button_pressed():
+	print("Resetting! Maybe...")
+
+
+func _on_key_1_pressed():
+	#Minigame_1.insert_into_next_slot("1");
+	pass # Replace with function body.
+
+
+func _on_key_2_pressed():
+	#Minigame_1.insert_into_next_slot("2");
+	pass # Replace with function body.
+
+
+func _on_key_3_pressed():
+	#Minigame_1.insert_into_next_slot("3");
+	pass # Replace with function body.
+
+
+func _on_key_4_pressed():
+	#Minigame_1.insert_into_next_slot("4");
+	pass # Replace with function body.
+
+
+func _on_key_5_pressed():
+	#Minigame_1.insert_into_next_slot("5");
+	pass # Replace with function body.
+
+
+func _on_key_6_pressed():
+	#Minigame_1.insert_into_next_slot("6");
+	pass # Replace with function body.
+
+
+func _on_key_7_pressed():
+	#Minigame_1.insert_into_next_slot("7");
+	pass # Replace with function body.
