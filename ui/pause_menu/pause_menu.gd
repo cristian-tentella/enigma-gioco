aa631a9bf27574e70cc2fb3b2a1b9d7085d1075c
Giorey01 <giorey2001@gmail.com>
2024-07-13 22:00:33 +0200

    Change function to call whenever quit game

--- ui/pause_menu/pause_menu.gd
+++ ui/pause_menu/pause_menu.gd
@@ -6,7 +6,7 @@ signal exit
 
 #Quando preme il tasto per tornare al menu principale
 func _on_main_menu_button_pressed():
-	SaveManager.save_current_state_into_json()
+	SaveManager.prepare_data_to_be_saved_and_save()
 	self.exit.emit()
 	GameManager.start()
 
