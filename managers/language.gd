4538c37c399e9de0d45cdb158c7bec1d4c5487db
Cristian Tentella <cristiantentella@tuta.com>
2024-07-16 18:29:15 +0200

    Refactor LanguageManager

--- managers/language.gd
+++ managers/language.gd
@@ -5,17 +5,12 @@ const ENGLISH = "en_US"
 const ITALIAN = "it_IT"
 
 
-func _ready():
-	save_language_to_state_manager()
-
-
 func get_language() -> String:
 	return TranslationServer.get_locale()
 
 
-func _set_language(language: String):
-	TranslationServer.set_locale(language)
-	save_language_to_state_manager()
+func change_language():
+	_set_language(_get_next_language())
 
 
 func save_language_to_state_manager():
@@ -26,8 +21,13 @@ func load_language_from_state_manager():
 	TranslationServer.set_locale(StateManager.current_language)
 
 
-func change_language():
-	_set_language(_get_next_language())
+func _ready():
+	save_language_to_state_manager()
+
+
+func _set_language(language: String):
+	TranslationServer.set_locale(language)
+	save_language_to_state_manager()
 
 
 func _get_next_language() -> String:
