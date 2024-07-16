81a90ce942a0ce7bd6628164a10b4d0f2a3eef46
Cristian Tentella <cristiantentella@tuta.com>
2024-07-16 17:19:07 +0200

    Remove debug print statements

--- managers/language.gd
+++ managers/language.gd
@@ -7,7 +7,6 @@ const ITALIAN = "it_IT"
 
 func _ready():
 	StateManager.current_language = TranslationServer.get_locale()
-	print_debug("La lingua attuale è " + StateManager.current_language)
 
 
 func _set_language(language: String):
@@ -16,9 +15,7 @@ func _set_language(language: String):
 
 
 func change_language():
-	print_debug("Prima di cambiare lingua quella attuale è " + StateManager.current_language)
 	_set_language(_get_next_language())
-	print_debug("La lingua è stata cambiata in " + StateManager.current_language)
 
 
 func _get_next_language() -> String:
