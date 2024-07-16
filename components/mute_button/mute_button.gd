2cb78ac495a862c5973d46f270188cb5db8569e8
Cristian Tentella <cristiantentella@tuta.com>
2024-07-16 19:14:38 +0200

    Save/load muted to state

--- components/mute_button/mute_button.gd
+++ components/mute_button/mute_button.gd
@@ -8,11 +8,13 @@ extends Control
 
 
 func _ready():
+	_load_muted_from_state_manager()
 	_update_icon()
 
 
 func _toggle_mute():
 	AudioServer.set_bus_mute(master_bus_index, not _is_muted())
+	_save_muted_to_state_manager()
 	_update_icon()
 
 
@@ -31,3 +33,11 @@ func _update_icon():
 	else:
 		unmuted_button.show()
 		muted_button.hide()
+
+
+func _save_muted_to_state_manager():
+	StateManager.muted = _is_muted()
+
+
+func _load_muted_from_state_manager():
+	AudioServer.set_bus_mute(master_bus_index, StateManager.muted)
