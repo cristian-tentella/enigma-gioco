47021dbe8e8b5cc7d67c28678b267c43f99a995a
Francesco Pittacolo <francesco.pittacolo@gmail.com>
2024-07-17 15:47:11 +0200

    Refactor

--- components/memeory/memeory.gd
+++ components/memeory/memeory.gd
@@ -2,11 +2,17 @@ extends Node
 
 class_name Memeory
 
+signal start
 signal update
 var slots: Array[Card] #Gli slot del memeory
 var picked: Array[Card]
 var clicks = 0
+var i = 0
 
+func start_game():
+	start.emit()
+	
+	
 func insert(card: Card):
 	slots.append(card)
 	update.emit()
@@ -28,8 +34,10 @@ func reset_pick():
 
 func remove_picks():
 	for card in picked:
+		#print_debug(card.index)
+		#print_debug(slots[card.index])
 		slots[card.index] = null
-		print_debug(slots)
+		#print_debug(slots)
 		update.emit()
 		
 func has_couple():
@@ -40,14 +48,22 @@ func check():
 	var check = has_couple()
 	if(!check):
 		await get_tree().create_timer(1).timeout
-		cover_cards()
+		cover_picked_cards()
 	else:
 		await get_tree().create_timer(0.7).timeout
 		remove_picks()
 	reset_pick()
 
-func cover_cards():
+func cover_picked_cards():
 	for card in MemeoryManager.picked:
 		card.update_card_sprite2D_back_texture()
 		update.emit()
 
+func cover_all_cards():
+	for card in MemeoryManager.slots:
+		card.update_card_sprite2D_back_texture()
+		update.emit()
+
+func clear_slots():
+	slots.clear()
+	update.emit()
