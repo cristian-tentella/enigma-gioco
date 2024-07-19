894f2119eb3c69a88960335dcfacbdb58c264015
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-07-19 13:16:13 +0200

    Add a comment

--- game/house/house.gd
+++ game/house/house.gd
@@ -4,4 +4,6 @@ extends Node2D
 func _ready():
 	"""SETUP PER PRIMO MINIGIOCO, ATTENZIONE A COME SONO MESSI I NODI NELLA SCENA!"""
 	MinigameManager.porta_camera_da_letto = $"Rooms/Cameretta + Terrazzo/porta_camera"
+	
+	"""SETUP PER SECONDO MINIGIOCO, ATTENZIONE A COME SONO MESSI I NODI NELLA SCENA!"""
 	MinigameManager.polipetto = $Rooms/Bagno/polipetto
