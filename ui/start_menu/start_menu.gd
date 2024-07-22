8950e08c59e3a381ccbd5c0d01a1f911b05e0209
Giorey01 <giorey2001@gmail.com>
2024-07-22 15:52:30 +0200

    Make the right sound track sounds both for menus and gameplay

--- ui/start_menu/start_menu.gd
+++ ui/start_menu/start_menu.gd
@@ -33,6 +33,7 @@ func show_logout_button():
 
 #Quando si clicca su play, viene caricato il salvataggio
 func _on_play_button_pressed():
+	AudioManager.stop_current_sound_track()
 	_hide_all_buttons()
 	_fade_in()
 	UIManager.show_loading_screen() #Fai vedere il loading screen
@@ -41,7 +42,7 @@ func _on_play_button_pressed():
 	UIManager.kil_loading_screen() #Sono uscito dal loading screen, quindi faccio killare l'elemento UI alla UIManager
 	_fade_out()
 	self.exit.emit()
-
+	AudioManager.play_main_theme_sound_track()
 
 func _fade_in():
 	fade.show()
@@ -91,6 +92,8 @@ func _on_resume_button_pressed():
 	UIManager.show_mobile_ui()
 	AudioManager.stop_current_sound_track()
 	self.exit.emit()
+	AudioManager.play_main_theme_sound_track()
+	
 
 
 
